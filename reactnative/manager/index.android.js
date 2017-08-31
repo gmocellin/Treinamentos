23bb550768a6cce5afe7d840db52447ba5cedb01
Giovane Cunha Mocellin <giovane.mocellin@gmail.com>
2017-08-31 18:36:40 -0300

    Mais reactnative

--- reactnative/manager/index.android.js
+++ reactnative/manager/index.android.js
@@ -1,5 +1,6 @@
 import { AppRegistry, UIManager } from 'react-native';
 import App from './src/App';
 
+console.ignoredYellowBox = ['Setting a timer'];
 UIManager.setLayoutAnimationEnabledExperimental(true);
 AppRegistry.registerComponent('manager', () => App);
--- reactnative/manager/src/components/EmployeeCreate.js
+++ reactnative/manager/src/components/EmployeeCreate.js
@@ -3,9 +3,14 @@ import { connect } from 'react-redux';
 import { Picker, Text, View } from 'react-native';
 
 import { Card, CardSection, TextField, Button } from './common';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 
 class EmployeeCreate extends Component {
+    onButtonPress(){
+        const { name, phone, shift } = this.props;
+        this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+    }
+
     render() {
         return (
             <Card>
@@ -47,7 +52,7 @@ class EmployeeCreate extends Component {
                 </CardSection>
 
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)} >
                         Create
                     </Button>
                 </CardSection>
@@ -72,4 +77,4 @@ const mapStateToProps = (state) => {
     return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, { employeeUpdate, employeeCreate })(EmployeeCreate);
